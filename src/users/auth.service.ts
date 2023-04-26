95269047bec08b9d109834df69d75647de73e833
ArtinaBektashi <ab51034@ubt-uni.net>
2023-04-26 10:10:51 +0200

    added password hashing and salt

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@nestjs/common/decorators";
 import { UsersService } from "./users.service";
-import { BadRequestException } from "@nestjs/common";
+import { BadRequestException, NotFoundException } from "@nestjs/common";
 import { randomBytes, scrypt as _scrypt } from "crypto";
 import { promisify } from "util";
 
@@ -34,7 +34,20 @@ export class AuthService{
         return user;
     }
 
-    signin(){
+    async signin(email:string, password:string){
+        const [user] = await this.usersService.find(email);
+        if(!user){
+            throw new NotFoundException('User not found');
+        }
 
+        const [salt, storedHash] = user.password.split('.');
+
+        const hash = (await scrypt(password,salt,32)) as Buffer;
+
+        if (storedHash !== hash.toString('hex')){
+            throw new BadRequestException('Bad password')
+        }
+
+        return user;
     }
 }
