ade52597f7e65d53661cc8b8225796ca2340e1c7
ArtinaBektashi <ab51034@ubt-uni.net>
2023-04-27 15:44:44 +0200

    estimate method for a vehicle

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, UseGuards, Patch , Param } from '@nestjs/common';
+import { Controller, Post, Body, UseGuards, Patch , Param, Query, Get } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import {AuthGuard} from '../guards/auth.guard';
@@ -8,6 +8,7 @@ import { ReportDto } from './dtos/report.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
 import { ApproveReportDto } from './dtos/approvereport.dto';
 import { AdminGuard } from 'src/guards/admin.guard';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -24,6 +25,11 @@ export class ReportsController {
     @UseGuards(AdminGuard)
     approveReport(@Param('id') id: string, @Body() body: ApproveReportDto){
         return this.reportsService.changeApproval(id, body.approved);
+    }
+
+    @Get()
+    getEstimate(@Query() query: GetEstimateDto){
+        return this.reportsService.createEstimate(query);
     }
 
 }
